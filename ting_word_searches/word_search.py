8f4668a5d35541e1f7ad11097d1b4c901025a8cc
analaurafr <laurafernandes@outlook.com>
2024-03-19 17:33:10 -0300

    testes

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        metadata = instance.search(i)
+        occurrences = []
+
+        for index, line in enumerate(metadata["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": metadata["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        metadata = instance.search(i)
+        occurrences = []
+
+        for index, line in enumerate(metadata["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": metadata["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
